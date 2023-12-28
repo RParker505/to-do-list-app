9fd76fb0ebd270d0fb2b552f04a415485eb77fdc
RParker505 <rchiado75@gmail.com>
2023-12-28 06:02:07 -0700

    Add newItem jQuery function

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+function newItem() {
+    //jquery
+    //1. Adding a new item to the list of items:
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+  
+    if (!inputValue){
+      alert("You must write something!");
+    } else {
+      let list = $('#list').append(li)
+      }
+    
+    //2. Crossing out an item from the list of items:
+    li.on('dblclick', function (event){
+      li.addClass('strike')
+    });
+    
+    //3(i). Adding the delete button "X":
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode("X"));
+       li.append(crossOutButton);
+    //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+    crossOutButton.on('click', function (event){
+       li.addClass('delete')
+    });
+    
+    // 4. Reordering the items:
+    $('#list').sortable();
+  }
